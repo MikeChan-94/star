5b2ad4ee300290fe6048ae04648b1dddd8bc93f0
chenweijian <361261189@qq.com>
2020-11-21 13:15:00 +0800

    配置druid面板

--- src/main/java/com/mikechen/star/StarApplication.java
+++ src/main/java/com/mikechen/star/StarApplication.java
@@ -3,9 +3,11 @@ package com.mikechen.star;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@ServletComponentScan
 @MapperScan("com.mikechen.star.mapper")
 @EnableSwagger2
 public class StarApplication {
--- src/main/java/com/mikechen/star/controller/CheckController.java
+++ src/main/java/com/mikechen/star/controller/CheckController.java
@@ -1,12 +1,12 @@
 package com.mikechen.star.controller;
 
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CheckController {
 
-    @PostMapping("/check")
+    @GetMapping("/check")
     public String check() {
         return "success";
     }
